de5b36b874a8807b8b45e56b1cf933e65a66b366
Niko Korkala <nikokorkala5@kamk.fi>
2023-09-03 12:31:50 +0300

    modify index.js

--- phonebook/index.js
+++ phonebook/index.js
@@ -126,7 +126,7 @@ app.use(morgan((tokens,request,response) => {
     {
       return response.status(400).send({ error: 'malformatted id'})
     }
-    else if(error.name === 'ValidationError' || error.number === 'ValidationError')
+    else if(error.name === 'ValidationError')
     {
       return response.status(400).json({ error: error.message })
     }
